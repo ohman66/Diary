a29e228805e3780d6bf60d2451b29ede4a8a202c
ohman66 <1968612619@qq.com>
2021-11-02 19:26:19 +0800

    完善界面

---
+++ app/src/main/java/android/mainactivity/diary.java
@@ -0,0 +1,53 @@
+package android.mainactivity;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
+
+public class diary extends LitePalSupport{
+
+    private String title;
+    private String content;
+    private String time;
+    private String author;
+
+    public diary(String title, String content, String time, String author) {
+        this.title = title;
+        this.content = content;
+        this.time = time;
+        this.author = author;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
